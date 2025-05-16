1f6e438588ff1b6c5125b0eddc6c2a7288bed213
Ernest Barrington <embarrington3@gmail.com>
2025-05-16 19:51:23 -0400

    Added questions from 5/14-5/16

---
+++ src/main/kotlin/111. Minimum Depth of Binary Tree.kt
@@ -0,0 +1,12 @@
+import DataStructure.TreeNode
+
+fun minDepth(root: TreeNode?): Int {
+    if(root == null) return 0;
+    if(root.left == null) return 1 + minDepth(root.right)
+    if(root.right == null) return 1 + minDepth(root.left)
+    return 1 + minOf(minDepth(root.left), minDepth(root.right))
+}
+
+fun main() {
+    //TODO: Test cases
+}
---
+++ src/main/kotlin/1768. Merge Strings Alternatively.kt
@@ -0,0 +1,18 @@
+fun mergeAlternately(word1: String, word2: String): String {
+    var i = 0
+    var res = ""
+    val n = maxOf(word1.length, word2.length)
+    while (i < n) {
+        if (i < word1.length) res += word1[i]
+        if (i < word2.length) res += word2[i]
+        i++
+    }
+
+    return res
+}
+
+fun main() {
+    println(mergeAlternately("abc", "pqr") == "apbqcr")
+    println(mergeAlternately("ab", "pqrs") == "apbqrs")
+    println(mergeAlternately("abcd", "pq") == "apbqcd")
+}
